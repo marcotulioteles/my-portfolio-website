1d0a5934f4f21b67d7a5455cd2e29b1096b5b418
Marco Túlio Teles <marcotuliocivileng@gmail.com>
2022-07-18 00:45:47 -0300

    feat: finish the form styles

--- src/pages/ContactMe/index.tsx
+++ src/pages/ContactMe/index.tsx
@@ -1,4 +1,4 @@
-import { Envelope } from 'phosphor-react';
+import { At, EnvelopeSimple, NotePencil, PaperPlaneTilt, Phone, User } from 'phosphor-react';
 import { SocialMediaGroup } from '../../components/SocialMediaGroup';
 import { WindowTitlebarPoints } from '../../components/WindowTitlebarPoints';
 import styles from './styles.module.scss';
@@ -11,24 +11,28 @@ export function ContactMe() {
         <SocialMediaGroup />
         <form action="" >
           <div className={styles.title}>
-            <Envelope size={54}/>
+            <EnvelopeSimple size={54} weight='light' />
             <h1>Contact me</h1>
           </div>
           <label htmlFor="userName">
-            <input type="text" name='userName'/>
+            <User size={18}/>
+            <input type="text" name='userName' placeholder='Name'/>
           </label>
           <div className={styles.inputGroup}>
             <label htmlFor="email">
-              <input type="email" name='email'/>
+              <At size={18}/>
+              <input type="email" name='email' placeholder='E-mail'/>
             </label>
             <label htmlFor="phone">
-              <input type="text" name='phone'/>
+              <Phone size={18}/>
+              <input type="text" name='phone' placeholder='Phone Number'/>
             </label>
           </div>
           <label htmlFor="message">
-            <textarea name="message" id="message"></textarea>
+            <NotePencil size={18}/>
+            <textarea name="message" id="message" rows={8} placeholder='Type your message here'></textarea>
           </label>
-          <button type='submit'>Send E-mail</button>
+          <button type='submit'><PaperPlaneTilt size={24} weight='light'/> Send E-mail</button>
         </form>
       </div>
     </div>
