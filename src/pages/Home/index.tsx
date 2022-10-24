11438d3be1d5e8f391c7132e165398ffe8321597
Marco Túlio Teles <marcotuliocivileng@gmail.com>
2022-10-24 12:05:52 -0300

    fix: use import ES6 Javascript to import pdf from assets and link it with anchor tag in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,15 @@
 import { DownloadSimple } from "phosphor-react";
-import { SiJavascript } from "react-icons/si";
-
+import myCurriculumPDF from '../../assets/CVMarcoTulio-en.pdf';
 import HomePhotoImg from "../../assets/marcotulio-home-image.png";
 import {
-  SOCIAL_MEDIA_LINKS_LIST,
   TECH_ICONS_DECORATION_LIST,
 } from "../../constants";
 import { useWindowSize } from "../../hooks/useWindowSize";
 import { SocialMediaGroup } from "../../components/SocialMediaGroup";
-import { SocialMediaLink } from "../../components/SocialMediaLink";
 import { TechIconDecoration } from "../../components/TechIconDecoration";
 import { WindowTitlebarPoints } from "../../components/WindowTitlebarPoints";
 
 import styles from "./styles.module.scss";
-import { INTLDiallingCodesSelect } from "../../components/INTLDiallingCodesSelect";
 
 export function Home() {
   const { width } = useWindowSize();
@@ -30,8 +26,8 @@ export function Home() {
           </h1>
           <span>Software Engineer Front-End</span>
           <a
-            href="/src/assets/CVMarcoTulio-en.pdf"
-            download
+            href={myCurriculumPDF}
+            download='CVMarcoTulio.pdf'
           >
             <DownloadSimple size={24} />
             Download CV
